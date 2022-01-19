f7a62a5cad180acb600978efd10769e5e11460bb
adnanaganovic <adnan1aga@gmail.com>
2022-01-19 20:59:06 +0100

    Moj prvi commit

--- src/eight/Collection/PersonL.java
+++ src/eight/Collection/PersonL.java
@@ -2,19 +2,22 @@ package eight.Collection;
 
 import seven.human.Gender;
 
-public class PersonL {
+import java.util.Objects;
+
+public class PersonL implements Comparable<PersonL> {
 
     private String name;
     private String surname;
     private int age;
     private Gender gender;
 
+
     public PersonL(String name, String surname, int age, Gender gender) {
-    this.name =name;
-    this.surname =surname;
-    this.age =age;
-    this.gender =gender;
-}
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.gender = gender;
+    }
 
 
     public String getName() {
@@ -53,7 +56,7 @@ public class PersonL {
 //    public String toString() {
 //        return "PersonL{" +
 //                "name='" + name + '\'' +
-//                ", surname='" + surname + '\'' +        //Ovaj Override je automatski InteliJ izbacio
+//                ", surname='" + surname + '\'' +        //Ovaj Override je automatski InteliJ izbacio pri tipkanju "toString"
 //                ", age=" + age +
 //                ", gender=" + gender +
 //                '}';
@@ -61,6 +64,30 @@ public class PersonL {
     @Override
     public String toString() {
         return name + " " + surname + ", age=" + age + ", gender=" + gender;    //Može i ovako jednostavniji pristup
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;          // ovo sve mi je IntelliJ ponudio automatski
+        PersonL personL = (PersonL) o;
+        return age == personL.age && Objects.equals(name, personL.name) && Objects.equals(surname, personL.surname) && gender == personL.gender;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, age, gender);                 //Josh Bloch kaže OVERRIDE hasCode i equals zajedno
+    }
+
+    @Override
+    public int compareTo(PersonL o) {                    // Compare to metodu zahtjeva implementacija Comparable interfejsa
+        if (age < o.getAge()) {
+            return -1;
+        }
+        if (age > o.getAge()) {
+            return 1;
+        }
+        return 0;
     }
 }
 
--- src/eight/Collection/Tester.java
+++ src/eight/Collection/Tester.java
@@ -2,6 +2,7 @@ package eight.Collection;
 
 import seven.human.Gender;
 
+import java.time.LocalDate;
 import java.util.*;
 
 public class Tester {
@@ -47,6 +48,8 @@ public class Tester {
 //            String element = names2.get(i);       //crveni se get ako nije Lista
 //            System.out.println(element);
 
+        System.out.println("************************");
+
         List<PersonL> persons = new ArrayList<>();
         PersonL p1 = new PersonL("Jasna", "Ibrahimpašić", 23, Gender.FEMALE);
         persons.add(p1);
@@ -62,8 +65,69 @@ public class Tester {
         persons.add(p6);
         PersonL p7 = new PersonL("Ahmed", "Babić", 30, Gender.MALE);
         persons.add(p7);
-for (PersonL imena: persons){                                // Ovo je to String metoda koja izbacuje haxadecimalni rezultat, potrebno je overridati ovu metodu to String u PersonList klasi
+        persons.add(p7);
+for (PersonL imena: persons){                                // Ovo je to String metoda koja izbacuje haxadecimalni rezultat, potrebno je overridati ovu metodu to String u PersonL klasi
     System.out.println(imena);
 }
+
+        System.out.println("***********************");
+
+
+        Set<PersonL> persons2 = new LinkedHashSet<>();
+        PersonL p21 = new PersonL("Jasna", "Ibrahimpašić", 23, Gender.FEMALE);
+        persons2.add(p21);
+        PersonL p22 = new PersonL("Adnan", "Aganović", 33, Gender.MALE);
+        persons2.add(p22);
+        PersonL p23 = new PersonL("Ademira", "Aganović", 37, Gender.FEMALE);
+        persons2.add(p23);
+        PersonL p24 = new PersonL("Aria", "Aganović", 2, Gender.FEMALE);
+        persons2.add(p24);
+        PersonL p25 = new PersonL("Refik", "Aganović", 69, Gender.MALE);
+        persons2.add(p25);
+        PersonL p26 = new PersonL("Mubera", "Babić", 55, Gender.FEMALE);
+        persons2.add(p26);
+        PersonL p27 = new PersonL("Haris", "Aganović", 28, Gender.MALE);
+        persons2.add(p27);
+        PersonL p28 = new PersonL("Ahmed", "Babić", 30, Gender.MALE);
+        persons2.add(p28);
+        persons2.add(p28);                                                                               //Ova tri dodana objekta imaju isti hashcode
+        persons2.add(p28);
+        persons2.add(new PersonL("Ahmed", "Babić", 30, Gender.MALE));                 // a ovaj kreirani u letu ima drugačiji hashcode iako se radi o istom objektu, ali na drugoj memorijskoj lokaciji
+        for (PersonL imena: persons2){                                                                // HashSet izbacuje duplikate sa istim hash kodom (adresom hasha)
+            System.out.println(imena);
+        }
+
+        System.out.println("**********************************");
+
+        Set<PersonL> persons3 = new TreeSet<>();                                                        //TreeSet da bi funkcionisao zahtjeva comparable objekat (clasu), pa je
+        PersonL p31 = new PersonL("Jasna", "Ibrahimpašić", 33, Gender.FEMALE);       // potrebno dodati u klasu PersonL implements comparable<PersonL>
+        persons3.add(p31);
+        PersonL p32 = new PersonL("Adnan", "Aganović", 33, Gender.MALE);              // comparable interfejs ima metodu compare to koja se mora implementirati
+        persons3.add(p32);
+        PersonL p33 = new PersonL("Ademira", "Aganović", 37, Gender.FEMALE);         //compareTo metoda mu je osnov da izbaci duplikate
+        persons3.add(p33);
+        PersonL p34 = new PersonL("Aria", "Aganović", 2, Gender.FEMALE);             //ako smo compareTo sortirali po godinama, samo će jedan objekat sa istim godinama prikazati
+        persons3.add(p34);
+        PersonL p35 = new PersonL("Refik", "Aganović", 69, Gender.MALE);
+        persons3.add(p35);
+        PersonL p36 = new PersonL("Mubera", "Babić", 55, Gender.FEMALE);
+        persons3.add(p36);
+        PersonL p37 = new PersonL("Haris", "Aganović", 28, Gender.MALE);
+        persons3.add(p37);
+        PersonL p38 = new PersonL("Ahmed", "Babić", 30, Gender.MALE);
+        persons3.add(p38);
+        persons3.add(p38);                                                                               //Ova tri dodana objekta imaju isti hashcode
+        persons3.add(p38);
+        persons3.add(new PersonL("Ahmed", "Babić", 30, Gender.MALE));                 // a ovaj kreirani u letu ima drugačiji hashcode iako se radi o istom objektu, ali na drugoj memorijskoj lokaciji
+        for (PersonL imena: persons3){                                                                // HashSet izbacuje duplikate sa istim hash kodom (adresom hasha)
+            System.out.println(imena);
+        }
+
     }
 }
+
+//
+//    public int getAge() {
+//        LocalDate now = LocalDate.now();
+//        int age = birthday.until(now).getYears();        //Računanje rođendana
+//        return age;
